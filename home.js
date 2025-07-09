1c408b753404816e1344d2988b6e313bb37e0fb6
oluwaduyilemi <favourbakare24@gmail.com>
2025-07-09 08:12:09 +0100

    service section done

--- home.js
+++ home.js
@@ -1,1 +1,8 @@
+// window.addEventListener('wheel', function(e) {
+//     if(e.deltaY !== 0) {
+//         document.querySelector('.nike-new').scrollLeft += e.deltaY;
+//           e.preventDefault();
+//     }
+// }, { passive: false });
+
 
