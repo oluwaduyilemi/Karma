9fe309f4334b0802c224bcc271e61b2a0e97342f
oluwaduyilemi <favourbakare24@gmail.com>
2025-07-18 22:21:54 +0100

    intial commit

--- home.js
+++ home.js
@@ -5,4 +5,24 @@
 //     }
 // }, { passive: false });
 
+  const slides = document.querySelectorAll('.shore-cont');
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle('active', i === index);
+    });
+  }
+
+  document.getElementById('nextBtn').addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  });
+
+  document.getElementById('prevBtn').addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  });
+
+
 
